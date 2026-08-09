d6389768841ae31f86bdd5f46a8775c94f285e33
SirBoely <183182665+SirBoely@users.noreply.github.com>
2026-08-09 21:26:12 +0200

    feat: add B-Trades executive agentic OS package

---
+++ btrades_executive_os/__init__.py
@@ -0,0 +1,23 @@
+"""B-Trades Executive Agentic OS extension for OpenJarvis."""
+
+from .core import (
+    AnalyticsAgent,
+    BusinessIntelligenceCapitalLeader,
+    ExecutiveTriage,
+    GoalTrackingAgent,
+    GrowthEngineeringLeader,
+    OperationsAgent,
+    ResourceManagementSystem,
+    TrustGovernanceOperationsLeader,
+)
+
+__all__ = [
+    "AnalyticsAgent",
+    "BusinessIntelligenceCapitalLeader",
+    "ExecutiveTriage",
+    "GoalTrackingAgent",
+    "GrowthEngineeringLeader",
+    "OperationsAgent",
+    "ResourceManagementSystem",
+    "TrustGovernanceOperationsLeader",
+]
